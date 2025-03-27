28301c1fba055be49fc04a402709ed960b8bd3d5
IndustryDev <industrydevpw@gmail.com>
2025-03-27 14:09:24 +0200

    commands.py

--- commands.py
+++ commands.py
@@ -1,1 +1,18 @@
 
+from aiogram.filters import Command
+from aiogram.types.bot_command import BotCommand
+
+BOOKS_COMMAND = Command("books")
+START_COMMAND = Command("start")
+SEARCH_BOOKS_COMMAND = Command("search_books")
+FILTER_BOOKS_COMMAND = Command("filter_books")
+BUY_BOOKS_COMMAND = Command("buy_book")
+
+BOT_COMMANDS = [
+   BotCommand(command="books", description="View a list of available books"),
+   BotCommand(command="start", description="Start a conversation"),
+   BotCommand(command="search_books", description="Search books"),
+   BotCommand(command="filter_books", description="Filter books"),
+   BotCommand(command="buy_book", description="Buy book")
+]
+
