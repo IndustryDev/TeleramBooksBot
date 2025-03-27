5b3da1f1dfc7d2be410f0b6b510ee22f2d6fb997
IndustryDev <industrydevpw@gmail.com>
2025-03-27 14:12:46 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
 
+import json
+
+from models import Book
+
+
+def get_books(file_path: str = "data.json", book_id: int | None = None) -> list[dict] | dict:
+    with open(file_path, "r") as fp:
+        books = json.load(fp)
+        if book_id is not None:
+            return books[book_id]
+        return books
