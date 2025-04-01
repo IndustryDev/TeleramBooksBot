422626a3c59678a5150a3b9ffbb5082417f59845
IndustryDev <industrydevpw@gmail.com>
2025-04-01 21:08:09 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
 
+from pydantic import BaseModel
+
+
+class Book(BaseModel):
+    id: int
+    name: str
+    description: str
+    rating: float
+    genre: str
+    author: str
+    cover: str
+    year: int
+    price: int
+    website: str
