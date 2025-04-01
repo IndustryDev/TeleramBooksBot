fed6c78187de4b9d6c3e30d4780c968840b2a399
IndustryDev <industrydevpw@gmail.com>
2025-04-01 21:08:21 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,1 +1,23 @@
+from aiogram.fsm.state import StatesGroup, State
 
+
+class BookSort(StatesGroup):
+    search_query = State()
+
+
+class BookForm(StatesGroup):
+    name = State()
+    description = State()
+    rating = State()
+    year = State()
+    genre = State()
+    author = State()
+    cover = State()
+    price = State()
+
+
+class BookFilter(StatesGroup):
+    filter_criteria = State()
+    rating = State()
+    year = State()
+    genre = State()
