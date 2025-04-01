dc96437a4d11cc999e6d362ee97138df397730a1
IndustryDev <industrydevpw@gmail.com>
2025-04-01 21:07:59 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,20 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.filters.callback_data import CallbackData
+
+
+class BookCallback(CallbackData, prefix="book", sep=";"):
+    id: int
+    name: str
+
+
+def books_keyboard_markup(books_list: list[dict], offset: int | None = None, skip: int | None = None):
+    builder = InlineKeyboardBuilder()
+    for book_data in books_list:
+        callback_data = BookCallback(**book_data)
+        builder.button(
+            text=f"{callback_data.name}",
+            callback_data=callback_data.pack()
+        )
+    builder.adjust(1, repeat=True)
+    return builder.as_markup()
 
